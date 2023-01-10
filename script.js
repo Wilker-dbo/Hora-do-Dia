cf18eb4df3f2eab3424ec5a49771b783df0bdea6
Wilker-dbo <118698227+Wilker-dbo@users.noreply.github.com>
2023-01-10 20:31:54 -0300

    new file:   "Manh\303\243.png" modified:   ex1.html new file:   noite.png modified:   script.js new file:   tarde.png

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12) {
     //BOM DIA!
-    img.src = '../Manhã.png'
+    img.src = './Manhã.png'
     document.body.style.background = '#FEEB4D'
     
 } else if (hora>= 12 && hora <18){
    //BOA TARDE!
-    img.src = '../tarde.png'
+    img.src = './tarde.png'
     document.body.style.background = '#FC9A2B'
 } else{
     //BOA NOITE!
-    img.src = '../noite.png'
+    img.src = './noite.png'
     document.body.style.background = '#003D63'
 }
 }
